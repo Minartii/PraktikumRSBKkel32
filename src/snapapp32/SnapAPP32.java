8028a321264d5de8dffd7b022673c4ec9125f1e8
minartii <matarigan@student.ce.undip.ac.id>
2018-10-29 11:26:29 +0700

    upload pertama

---
+++ src/snapapp32/SnapAPP32.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snapapp32;
+
+/**
+ *
+ * @author Asus
+ */
+public class SnapAPP32 extends SnapFrame{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SnapAPP32 a = new SnapAPP32();
+        a.setVisible(true);
+    }
+    
+}
